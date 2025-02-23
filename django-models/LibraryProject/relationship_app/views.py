6ab5ee34200a5abee7b1e539135ceb4d4dfaf574
unknown <stevendbanda@yahoo.comgit config --global user.name>
2025-02-24 00:41:35 +0400

    adding html

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -14,5 +14,3 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'library_detail.html'
     context_object_name = 'library'
-    ["relationship_app/library_detail.html"]
-    
