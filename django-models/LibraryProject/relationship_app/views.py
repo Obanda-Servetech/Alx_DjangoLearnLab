da2a3a9cc388088af61b6971bfefb15cbfe77841
unknown <stevendbanda@yahoo.comgit config --global user.name>
2025-02-24 01:00:28 +0400

    Added user authentication views and templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -17,7 +17,38 @@ class LibraryDetailView(DetailView):
     ["relationship_app/list_books.html"]
     ["relationship_app/library_detail.html"]
     ["from django.views.generic.detail import DetailView"]
-    
+
+    from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('list_books')  # Redirect to a valid view after login
+    else:
+        form = AuthenticationForm()
+    return render(request, 'relationship_app/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return render(request, 'relationship_app/logout.html')
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log in the user automatically after registration
+            return redirect('list_books')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+
 
 
 
