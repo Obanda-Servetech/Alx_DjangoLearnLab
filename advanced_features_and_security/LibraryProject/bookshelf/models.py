7751c3b1916f4a6f162766dafbb0e4e3648bca47
unknown <stevendbanda@yahoo.comgit config --global user.name>
2025-02-24 08:25:36 +0400

    adding advanced features

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -13,4 +13,19 @@ class Book(models.Model):
 
 ["class CustomUserManager(BaseUserManager):", "create_user", "create_superuser"]
 
+# Defining custom permissions in models
+from django.db import models
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view articles"),
+            ("can_create", "Can create articles"),
+            ("can_edit", "Can edit articles"),
+            ("can_delete", "Can delete articles"),
+        ]
+
 
