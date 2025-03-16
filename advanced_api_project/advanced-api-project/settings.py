a74769a7a35664e9d551b18e884870111ee591a7
unknown <stevendbanda@yahoo.comgit config --global user.name>
2025-03-17 03:31:26 +0400

    adding django_filters

--- advanced_api_project/advanced-api-project/settings.py
+++ advanced_api_project/advanced-api-project/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -37,9 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework' #Add this line
-    'api'
-    ''
+    'rest_framework', #Add this line
+    'api',
+    'django_models',
+    
 ]
 
 MIDDLEWARE = [
