8c8ddbfc76a566082574adb7360e61dedeb04779
unknown <stevendbanda@yahoo.comgit config --global user.name>
2025-03-30 23:02:11 +0200

    static files update

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -126,22 +126,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 import os
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'blog/static'),
-]
-import os
-
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # For production
 
 TEMPLATES = [
     {
