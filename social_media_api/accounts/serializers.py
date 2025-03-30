9c72bbfb0a9b7b858c2255552dccfacf4d9c686a
unknown <stevendbanda@yahoo.comgit config --global user.name>
2025-03-31 01:38:18 +0200

    updating serialiers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -7,3 +7,5 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
+
+["from rest_framework.authtoken.models import Token", "serializers.CharField()", "Token.objects.create", "get_user_model().objects.create_user"]
