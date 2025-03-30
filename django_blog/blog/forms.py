f30fb05b40986e671a89b30a847180b2fd6fc435
unknown <stevendbanda@yahoo.comgit config --global user.name>
2025-03-31 00:54:47 +0200

    updating forms,py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -8,3 +8,27 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['email']  # Add other fields here, e.g., profile picture
+
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
+from django import forms
+from .models import Comment
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+        }
+["CommentForm(forms.ModelForm)", "model = Comment", "content"]
