78c48a5ae22b2f57c84fc1e84acd08fd73367ddc
unknown <stevendbanda@yahoo.comgit config --global user.name>
2025-03-30 23:47:04 +0200

    adding post model deatls

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,13 +1,15 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from django.contrib.auth.models import User
 
 class Post(models.Model):
     """
     Model representing a blog post.
     """
+    STATUS_CHOICES = (
+        (True, "Published"),
+        (False, "Draft"),
+    )
+
     title = models.CharField(max_length=200, help_text="Enter the title of the blog post.")
     content = models.TextField(help_text="Write the content of the blog post here.")
     published_date = models.DateTimeField(auto_now_add=True, help_text="Date and time when the post was published.")
@@ -17,9 +19,11 @@ class Post(models.Model):
         related_name="posts",
         help_text="The author of the blog post. A user can have multiple posts."
     )
+    status = models.BooleanField(choices=STATUS_CHOICES, default=False, help_text="Publish status of the post (Draft or Published).")
 
     class Meta:
         ordering = ["-published_date"]  # Latest posts appear first
 
     def __str__(self):
         return self.title
+
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -49,3 +49,45 @@ def edit_profile(request):
     else:
         form = ProfileEditForm(instance=request.user)
     return render(request, 'edit_profile.html', {'form': form})
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from .models import Post
+from .forms import PostForm
+
+# List all blog posts
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+    context_object_name = "posts"
+    ordering = ["-published_date"]
+
+# View a single post
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
+
+# Create a new post
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = "blog/post_form.html"
+    success_url = reverse_lazy("post_list")
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user  # Assign current user as author
+        return super().form_valid(form)
+
+# Update an existing post
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = "blog/post_form.html"
+    success_url = reverse_lazy("post_list")
+
+# Delete a post
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    template_name = "blog/post_confirm_delete.html"
+    success_url = reverse_lazy("post_list")
